1be27f83eed54fef62320c7429b3eeaba54a6aa4
milser <ruben2work@gmail.com>
2024-04-08 18:37:17 +0200

    v0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='edastatmil_milser',
-    version='0.8',
+    version='0.9',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     author='Tatiana Cazorla y Rubén Serrano',
